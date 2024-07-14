824f7085f9e0787f97ef9afd11cb5320a2cd6208
DrMint <29893320+DrMint@users.noreply.github.com>
2024-07-14 09:07:12 +0200

    Revert changes

--- payload/webhooks.ts
+++ payload/webhooks.ts
@@ -1,51 +1,8 @@
 import type { Collections } from "./constants";
 
 export type AfterOperationWebHookMessage = {
-  operation: "update" | "create" | "delete";
-  addedDependantIds: string[];
-  urls: string[];
-} & (
-  | {
-      collection: Collections.WebsiteConfig;
-    }
-  | {
-      collection: Collections.Pages;
-      id: string;
-      slug: string;
-    }
-  | {
-      collection: Collections.Collectibles;
-      id: string;
-      slug: string;
-    }
-  | {
-      collection: Collections.Folders;
-      id: string;
-      slug: string;
-    }
-  | {
-      collection:
-        | Collections.Attributes
-        | Collections.Audios
-        | Collections.ChronologyEvents
-        // | Collections.Collectibles
-        | Collections.CreditsRole
-        | Collections.Currencies
-        | Collections.Files
-        // | Collections.Folders
-        | Collections.GenericContents
-        | Collections.Images
-        | Collections.Languages
-        | Collections.MediaThumbnails
-        // | Collections.Pages
-        | Collections.Recorders
-        | Collections.Scans
-        | Collections.Tags
-        | Collections.Videos
-        | Collections.VideosChannels
-        | Collections.VideosSubtitles
-        | Collections.Wordings
-        // | Collections.WebsiteConfig;
-      id: string;
-    }
-);
+    collection: Collections;
+    id?: string;
+    addedDependantIds: string[];
+    urls: string[];
+  };
