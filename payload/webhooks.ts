3582a48bd12a66e99121d9fbc01681971a7d943a
DrMint <29893320+DrMint@users.noreply.github.com>
2024-07-25 00:06:39 +0200

    Updated webhook type

--- payload/webhooks.ts
+++ payload/webhooks.ts
@@ -1,8 +1,24 @@
-import type { Collections } from "./constants";
-
-export type AfterOperationWebHookMessage = {
-  collection: Collections;
-  id?: string;
-  addedDependantIds: string[];
-  urls: string[];
-};
+export type EndpointChange = {
+  url: string;
+} & (
+  | { type: "getConfig" }
+  | { type: "getFolder"; slug: string }
+  | { type: "getLanguages" }
+  | { type: "getCurrencies" }
+  | { type: "getWordings" }
+  | { type: "getPage"; slug: string }
+  | { type: "getCollectible"; slug: string }
+  | { type: "getCollectibleScans"; slug: string }
+  | { type: "getCollectibleScanPage"; slug: string; index: string }
+  | { type: "getCollectibleGallery"; slug: string }
+  | { type: "getCollectibleGalleryImage"; slug: string; index: string }
+  | { type: "getChronologyEvents" }
+  | { type: "getChronologyEventByID"; id: string }
+  | { type: "getImageByID"; id: string }
+  | { type: "getAudioByID"; id: string }
+  | { type: "getVideoByID"; id: string }
+  | { type: "getFileByID"; id: string }
+  | { type: "getRecorderByID"; id: string }
+  | { type: "getAllSdkUrls" }
+  | { type: "getAllIds" }
+);
